0968717e5493eaa7763bae68154e38c6c2e9e2ee
Junior Penn <pennchojunior@gmail.com>
2025-01-06 23:02:46 +0900

    Abouts page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :bike
-
+  has_one_attached :photo
   validates :reviewer_name, presence: true
   validates :rating, presence: true, inclusion: { in: 1..5 }
   validates :content, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -35,13 +35,12 @@ require "open-uri"
 
 # puts "Categories created: #{Category.count}"
 
-
-brands = ["Honda", "Yamaha", "Kawasaki", "BMW", "Suzuki", "Ducati", "Segway", "Can-Am", "CFMOTO",
-            "Polaris", "Harley Davidson", "Royal Enfield", "Indian", "KYMCO", "Piaggio", "SYM", "Harley", "KTM", "Steve McQueen", "Arctic Cat" ]
-brands.map do |brand_name|
-    Brand.find_or_create_by!(name: brand_name)
-end
-  puts "Brands created: #{Brand.count}"
+# brands = ["Honda", "Yamaha", "Kawasaki", "BMW", "Suzuki", "Ducati", "Segway", "Can-Am", "CFMOTO",
+#             "Polaris", "Harley Davidson", "Royal Enfield", "Indian", "KYMCO", "Piaggio", "SYM", "Harley", "KTM", "Steve McQueen", "Arctic Cat" ]
+# brands.map do |brand_name|
+#     Brand.find_or_create_by!(name: brand_name)
+# end
+#   puts "Brands created: #{Brand.count}"
 
 # User.destroy_all
 # puts "All users destroyed."
@@ -72,4 +71,4 @@ end
 # require_relative "seeds/utvs"
 # require_relative "seeds/electric"
 # require_relative "seeds/reviews"
-require_relative "seeds/articles"
+# require_relative "seeds/articles"
